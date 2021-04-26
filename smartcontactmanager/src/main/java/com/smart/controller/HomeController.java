6822352340d7734674a63eee2df12a2c1c959339
Yash <yk.gautam97@gmail.com>
2021-04-26 13:56:20 +0530

    added the user registration process

--- smartcontactmanager/src/main/java/com/smart/controller/HomeController.java
+++ smartcontactmanager/src/main/java/com/smart/controller/HomeController.java
@@ -1,11 +1,24 @@
 package com.smart.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.smart.dao.UserRepository;
+import com.smart.entities.User;
+import com.smart.helper.Message;
 
 @Controller
 public class HomeController {
+
+	@Autowired
+	private UserRepository userRepository;
 
 	@RequestMapping(value = "/")
 	public String home(Model model) {
@@ -22,20 +35,42 @@ public class HomeController {
 	@RequestMapping(value = "/signup")
 	public String signup(Model model) {
 		model.addAttribute("title", "Register - smart contact manager");
+		model.addAttribute("user", new User());
 		return "signup";
 	}
 
-	/*
-	 * @Autowired private UserRepository UserRepository;
-	 * 
-	 * @GetMapping("/test")
-	 * 
-	 * @ResponseBody public String test() { User user=new User();
-	 * user.setName("Yash"); user.setEmail("yash@gmail.com"); Contact contact=new
-	 * Contact();
-	 * 
-	 * user.getContacts().add(contact); UserRepository.save(user); return "working";
-	 * }
-	 */
+//	handler for register
+
+	@RequestMapping(value = "/do_register", method = RequestMethod.POST)
+	public String registerUrl(@ModelAttribute("user") User user,
+			@RequestParam(value = "agreement", defaultValue = "false") boolean agreement, Model model,
+			HttpSession session) {
+
+		try {
+
+			if (!agreement) {
+				System.out.println("you have not agreed the terms and conditions");
+				throw new Exception("you have not agreed the terms and conditions");
+			}
+
+			user.setRole("ROLE_USER");
+			user.setEnabled(true);
+
+			System.out.println("Agreement " + agreement);
+			System.out.println("user " + user);
+
+			User result = this.userRepository.save(user);
+			model.addAttribute("user", result);
+			session.setAttribute("message", new Message("succefully registered ", "alert-success"));
+			return "signup";
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			model.addAttribute("user", user);
+			session.setAttribute("message", new Message("something went wrong ", "alert-danger"));
+			return "signup";
+		}
+
+	}
 
 }
